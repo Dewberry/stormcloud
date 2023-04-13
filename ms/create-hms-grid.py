7ebab706fee26bc2412613098088e8c8bb82ca54
Eric King <eking@dewberry.com>
2023-04-13 16:34:15 +0000

    add meilisearch scripts

---
+++ ms/create-hms-grid.py
@@ -0,0 +1,109 @@
+import os
+import boto3
+from meilisearch import Client
+from datetime import datetime
+import json
+import pyproj
+from pydsstools.heclib.dss import HecDss
+from pydsstools.heclib.utils import SHG_WKT
+from shapely.geometry import Point
+from shapely.ops import transform
+
+session = boto3.session.Session()
+bucket_name = os.environ["S3_BUCKET_NAME"]
+s3_client = session.client("s3")
+
+
+ms_client = Client(os.environ["REACT_APP_MEILI_HOST"], api_key=os.environ["REACT_APP_MEILI_MASTER_KEY"])
+index_name = "events"
+
+data_dir = "IndianCreek"
+grid_file = os.path.join(data_dir, "IC_Transpose.grid")
+dss_dir = os.path.join(data_dir, "dss")
+
+wgs84 = pyproj.CRS("EPSG:4326")
+project = pyproj.Transformer.from_crs(wgs84, SHG_WKT, always_xy=True).transform
+
+
+docs = ms_client.index(index_name).search(
+    "",
+    {
+        "filter": [
+            f'stats.mean >= 3.491 AND ranks.declustered_rank >= 1 AND metadata.watershed_name = "Indian Creek" AND metadata.transposition_domain_name = "v01"'
+        ],
+        "limit": 500,
+        "sort": ["stats.mean:desc", "start.timestamp:asc"],
+    },
+)["hits"]
+
+
+# make grid file
+with open(grid_file, "w") as gridf:
+    gridf.write("Grid Manager: IC Transpose\n")
+    gridf.write("     Version: 4.11\n")
+    gridf.write("     Filepath Separator: \\\n")
+    gridf.write("End:\n\n")
+
+    for i, doc in enumerate(docs):
+
+        s3_uri = os.path.join(
+            doc["metadata"]["transposition_domain_source"].replace(".geojson", ""),
+            f"{doc['duration']}h",
+            "dss",
+            f"{doc['id'].split('_')[-1]}.dss",
+        )
+        s3_key = s3_uri.replace(f"s3://{bucket_name}/", "")
+        dss_name = f"{doc['id'].split('_')[-1]}_Y{doc['ranks']['declustered_rank']:03}_T{i+1:03}.dss"
+        dss_path = os.path.join(dss_dir, dss_name)
+
+        s3_client.download_file(bucket_name, s3_key, dss_path)
+
+        with HecDss.Open(dss_path) as dss:
+            dss_pathname = dss.getPathnameList("/*/*/*/*/*/*/", sort=1)[0]
+
+        dss_filename = os.path.join("C:/Data", dss_path).replace("/", "\\")
+
+        last_modified = s3_client.head_object(Bucket=bucket_name, Key=s3_key)["LastModified"]
+        last_modified_date = datetime.strftime(last_modified, "%d %B %Y")
+        last_modified_time = datetime.strftime(last_modified, "%H:%M:%S")
+
+        # transform center to SHG
+        wgs_point = Point(doc["geom"]["center_x"], doc["geom"]["center_y"])
+        shg_point = transform(project, wgs_point)
+
+        # get grid info
+        grid = f"{doc['metadata']['source']} {doc['start']['datetime'].split(' ')[0]} Y{doc['ranks']['declustered_rank']:03} T{i+1:03}"
+        grid_type = "Precipitation"
+        ref_height_units = "Meters"
+        ref_height = "10.0"
+        data_source_type = "External DSS"
+        variant = "Variant-1"
+        default_variant = "Yes"
+        end_variant = "Variant-1"
+        use_lookup_table = "No"
+        storm_center_x = shg_point.x
+        storm_center_y = shg_point.y
+
+        # write grid info
+        gridf.write(f"Grid: {grid}\n")
+
+        gridf.write(f"     Grid Type: {grid_type}\n")
+        gridf.write(f"     Last Modified Date: {last_modified_date}\n")
+        gridf.write(f"     Last Modified Time: {last_modified_time}\n")
+        gridf.write(f"     Reference Height Units: {ref_height_units}\n")
+        gridf.write(f"     Reference Height: {ref_height}\n")
+        gridf.write(f"     Data Source Type: {data_source_type}\n")
+        gridf.write(f"     Variant: {variant}\n")
+
+        gridf.write(f"       Last Variant Modified Date: {last_modified_date}\n")
+        gridf.write(f"       Last Variant Modified Time: {last_modified_time}\n")
+        gridf.write(f"       Default Variant: {default_variant}\n")
+        gridf.write(f"       DSS File Name: {dss_filename}\n")
+        gridf.write(f"       DSS Pathname: {dss_pathname}\n")
+
+        gridf.write(f"     End Variant: {end_variant}\n")
+        gridf.write(f"     Use Lookup Table: {use_lookup_table}\n")
+        gridf.write(f"     Storm Center X: {storm_center_x}\n")
+        gridf.write(f"     Storm Center Y: {storm_center_y}\n")
+
+        gridf.write(f"End:\n\n")
---
+++ ms/meilisearch-upload.py
@@ -0,0 +1,162 @@
+import boto3
+from datetime import datetime, timedelta
+from dotenv import load_dotenv, find_dotenv
+import json
+from meilisearch import Client
+import numpy as np
+import os
+from scipy.stats import rankdata
+
+load_dotenv(find_dotenv())
+session = boto3.session.Session(os.environ["AWS_ACCESS_KEY_ID"], os.environ["AWS_SECRET_ACCESS_KEY"])
+s3_client = session.client("s3")
+
+s3_bucket = "tempest"
+
+index_name = "events"
+ms_client = Client(os.environ["REACT_APP_MEILI_HOST"], api_key=os.environ["REACT_APP_MEILI_MASTER_KEY"])
+
+year_range = range(1979, 2023)
+
+# watershed_domain = "kanawha/kanawha-transpo-area-v01"
+# watershed_domain = "upper-green-1404/upper-green-transpo-area-v01"
+# watershed_domain = "indian-creek/iowa-pilot"
+watershed_domain = "indian-creek/indian-creek-transpo-area-v01"
+
+
+# build meilisearch index
+# ms_client.index(index_name).delete()
+# ms_client.create_index(index_name, {"primaryKey": "start.timestamp"})
+# ms_client.create_index(index_name, {"primaryKey": "id"})
+
+
+docs = []
+for year in year_range:
+    print(year)
+    s3_prefix = f"watersheds/{watershed_domain}/72h/docs/{year}"
+
+    # get all s3 keys for year
+    results = s3_client.list_objects_v2(Bucket=s3_bucket, Prefix=s3_prefix)
+    s3_keys = [content["Key"] for content in results["Contents"]]
+
+    while "NextContinuationToken" in results.keys():
+        results = s3_client.list_objects_v2(
+            Bucket=s3_bucket, Prefix=s3_prefix, ContinuationToken=results["NextContinuationToken"]
+        )
+        s3_keys.extend([content["Key"] for content in results["Contents"]])
+
+    # read in json files for year
+    for key in s3_keys:
+        result = s3_client.get_object(Bucket=s3_bucket, Key=key)
+        doc = json.loads(result["Body"].read().decode())
+
+        # add png url
+        doc_meta = doc["metadata"]
+        doc_meta[
+            "png"
+        ] = (
+            png_url
+        ) = f"https://tempest.s3.amazonaws.com/watersheds/{watershed_domain}/72h/pngs/{doc['start']['datetime'].split(' ')[0].replace('-', '')}.png"
+        doc["metadata"] = doc_meta
+
+        if watershed_domain == "indian-creek/iowa-pilot":
+            doc_start = doc["start"]
+            doc_start["timestamp"] = int(doc_start["timestamp"])
+        doc["start"] = doc_start
+        doc_cats = {
+            "lv10": doc_meta["watershed_name"],
+            "lv11": f"{doc_meta['watershed_name']} > {doc_meta['transposition_domain_name']}",
+        }
+        doc["categories"] = doc_cats
+
+        # add id
+        doc[
+            "id"
+        ] = f'{doc["metadata"]["watershed_name"].lower().replace(" ","-")}_{doc["metadata"]["transposition_domain_name"].lower()}_{doc["duration"]}h_{doc["start"]["datetime"].split(" ")[0].replace("-", "")}'
+
+        docs.append(doc)
+
+
+# rank docs by year
+docs = np.array(docs)
+starts = np.array([datetime.strptime(d["start"]["datetime"], "%Y-%m-%d %H:%M:%S") for d in docs])
+means = np.array([d["stats"]["mean"] for d in docs])
+mean_ranks = rankdata(means * -1, method="ordinal")
+
+# date decluster
+for i in range(1, len(mean_ranks) + 1):
+    idx = np.where(mean_ranks == i)
+    dt = starts[idx][0]
+    # print(i)
+    if i == 1:
+        decluster_mask = np.array([True])
+        starts_by_mean = np.array([dt])
+    else:
+        min_dt = dt - timedelta(hours=71)
+        max_dt = dt + timedelta(hours=71)
+
+        if np.any((starts_by_mean[decluster_mask] >= min_dt) & (starts_by_mean[decluster_mask] <= max_dt)):
+            decluster = False
+        else:
+            decluster = True
+
+        decluster_mask = np.append(decluster_mask, decluster)
+        starts_by_mean = np.append(starts_by_mean, dt)
+
+# is there a way I can give rank for everything and then have a decluster dates check box
+# essentially use two different ranks values
+years = np.array([dt.year for dt in starts])
+years_by_mean = np.array([dt.year for dt in starts_by_mean])
+
+# get true ranks and declustered rank for each date
+
+for year in year_range:
+    print(year)
+    yr_decluster_mask = decluster_mask[years_by_mean == year]
+    yr_starts_by_mean = starts_by_mean[years_by_mean == year]
+    yr_starts = starts[years == year]
+    yr_docs = docs[years == year]
+
+    ranked_docs = []
+    for doc, start in zip(yr_docs, yr_starts):
+        idx = np.where(yr_starts_by_mean == start)[0][0]
+
+        if yr_decluster_mask[idx]:
+            decluster_rank = np.where(yr_starts_by_mean[yr_decluster_mask] == start)[0][0] + 1
+        else:
+            decluster_rank = -1
+
+        true_rank = idx + 1
+
+        doc["ranks"] = {
+            "true_rank": int(true_rank),
+            "declustered_rank": int(decluster_rank),
+        }
+
+        ranked_docs.append(doc)
+
+    ms_client.index(index_name).add_documents(ranked_docs)
+
+
+# filterable attributes
+ms_client.index(index_name).update_filterable_attributes(
+    [
+        "start.calendar_year",
+        "start.water_year",
+        "start.season",
+        "duration",
+        "metadata.source",
+        "metadata.watershed_name",
+        "metadata.transposition_domain_name",
+        "ranks.true_rank",
+        "ranks.declustered_rank",
+        "categories.lv10",
+        "categories.lv11",
+        "stats.mean",
+    ]
+)
+
+# sortable attributes
+ms_client.index(index_name).update_sortable_attributes(
+    ["start.timestamp", "stats.mean", "stats.max", "stats.norm_mean", "stats.sum"]
+)

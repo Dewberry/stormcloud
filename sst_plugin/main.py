085477eecc555a3763f47101e91dcef19157fc62
Nick Roberts <njroberts@dewberry.com>
2023-10-27 18:13:40 -0400

    Add sst processing plugin for process api

---
+++ sst_plugin/main.py
@@ -0,0 +1,22 @@
+"""
+Main script for process api plugin for SST modeling
+"""
+
+import sys
+
+from papipyplug import parse_input, plugin_logger, print_results
+
+from .extract_storms_plugin_module import PLUGIN_PARAMS, main
+
+if __name__ == "__main__":
+    # Start plugin logger
+    plugin_logger()
+
+    # Read, parse, and verify input parameters
+    input_params = parse_input(sys.argv, PLUGIN_PARAMS)
+
+    # Add main function here
+    results = main(input_params)
+
+    # Print Results
+    print_results(results)

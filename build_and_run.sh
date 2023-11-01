8289db39fadcf8fa186f5a67e95ee1791c02ac46
Nick Roberts <njroberts@dewberry.com>
2023-11-01 13:35:26 -0400

    Update file paths used in shell

--- build_and_run.sh
+++ build_and_run.sh
@@ -2,15 +2,15 @@
 
 PLUGIN=$1
 
-if [[ $PLUGIN == "sst" ]]; then  
-    echo building $PLUGIN 
+if [[ $PLUGIN == "sst" ]]; then
+    echo building $PLUGIN
     docker build . -t sst-plugin -f Dockerfile.sst
-    docker run --rm -it --env-file plugins/sst/.env sst-plugin:latest
-elif [[ $PLUGIN == "temp_precip" ]]; then  
-    echo building $PLUGIN 
+    docker run --rm -it --env-file stormcloud/plugins/sst/.env sst-plugin:latest
+elif [[ $PLUGIN == "temp_precip" ]]; then
+    echo building $PLUGIN
     docker build . -t temp-precip-plugin -f Dockerfile.temp_precip
-    docker run --rm -it --env-file plugins/temp_precip/.env temp-precip-plugin:latest
+    docker run --rm -it --env-file stormcloud/plugins/temp_precip/.env temp-precip-plugin:latest
 else
   echo "plugin arg must be one of [sst, temp_precip]"
-fi 
+fi
 

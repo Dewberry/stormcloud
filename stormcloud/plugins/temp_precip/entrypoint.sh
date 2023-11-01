935f1f153afb1c79b40ec1f55b3ffb2a787b56ca
Seth Lawler <slawler@dewberry.com>
2023-11-01 15:40:42 -0400

    fix extra token in actions

--- stormcloud/plugins/temp_precip/entrypoint.sh
+++ stormcloud/plugins/temp_precip/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo 'Temperature and precipitation DSS extraction plugin for AORC .zarr data'
-python -m main "{\"start_date\": \"1979-02-02\", \"end_date\": \"1979-02-02\", \"watershed_name\": \"Trinity\", \"data_variables\": [\"APCP_surface\", \"TMP_2maboveground\"], \"zarr_s3_bucket\": \"tempest\", \"watershed_uri\": \"s3://tempest/watersheds/trinity/trinity-transpo-area-v00.geojson\", \"output_s3_bucket\": \"tempest\", \"output_s3_prefix\": \"watersheds/trinity/trinity-transpo-area-v00/por\"}"
+python -m main "{\"start_date\": \"1979-02-02\", \"end_date\": \"1979-02-03\", \"watershed_name\": \"Trinity\", \"data_variables\": [\"APCP_surface\", \"TMP_2maboveground\"], \"zarr_s3_bucket\": \"tempest\", \"watershed_uri\": \"s3://tempest/watersheds/trinity/trinity-transpo-area-v00.geojson\", \"output_s3_bucket\": \"tempest\", \"output_s3_prefix\": \"watersheds/trinity/trinity-transpo-area-v00/por\"}"

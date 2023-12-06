369679b219527d923bb13939ca56ec8fe23bfe04
Nick Roberts <njroberts@dewberry.com>
2023-12-06 05:01:10 +0000

    Make real example

--- stormcloud/plugins/hms_grid/entrypoint.sh
+++ stormcloud/plugins/hms_grid/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 echo 'GRID generation plugin for s3 DSS data'
-python -m main "{\"metadata_s3_uris\": [\"s3://bucket/key_metadata.json\"], \"watershed\": \"Duwamish\", \"output_zip_s3_uri"\: \"s3://bucket/key.zip\"}"
+python -m main "{\"metadata_s3_uris\": [\"s3://tempest/watersheds/duwamish/duwamish-transpo-area-v01/standardized_metadata/19941026_19941029_SST_metadata.json\"], \"watershed\": \"Duwamish\", \"output_zip_s3_uri\": \"s3://tempest/deliverables/duwamish_hms_grid_test.zip\"}"

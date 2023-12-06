bf454cba6959a5e5f8e61fc16fcdedfcee8bd1ba
Nick Roberts <njroberts@dewberry.com>
2023-12-06 05:01:29 +0000

    Minor syntax and input fixes

--- stormcloud/plugins/hms_grid/hms_grid_plugin.py
+++ stormcloud/plugins/hms_grid/hms_grid_plugin.py
@@ -8,7 +8,7 @@ from create_hms_grid import write_meta_to_grid
 from dotenv import load_dotenv
 from common.cloud import split_s3_path
 
-PLUGIN_PARAMS = {"required": ["metadata_s3_uris", "watershed", "output_zip_s3_uri"]}
+PLUGIN_PARAMS = {"required": ["metadata_s3_uris", "watershed", "output_zip_s3_uri"], "optional": []}
 
 
 def create_grid_filename(watershed: str) -> str:
@@ -38,13 +38,14 @@ def main(params: dict) -> dict:
     results_dict = {}
 
     with TemporaryDirectory() as tmp_dir:
-        full_directory_path = os.path.join(tmp_dir, params["directory_name"])
+        full_directory_path = os.path.join(tmp_dir, params["watershed"])
         grid_fn = create_grid_filename(params["watershed"])
         write_meta_to_grid(full_directory_path, grid_fn, params["metadata_s3_uris"], s3_client)
         zip_bucket, zip_key = split_s3_path(params["output_zip_s3_uri"])
         zip_path = os.path.basename(zip_key).replace(".zip", "")
-        output_zip = make_archive(zip_path, "zip", tmp_dir, full_directory_path)
-        s3_client.upload_file(zip_bucket, zip_key, output_zip)
+        output_zip = make_archive(zip_path, "zip", tmp_dir, params["watershed"])
+        logging.info(f"uploading zip output to {output_zip}")
+        s3_client.upload_file(output_zip, zip_bucket, zip_key)
         results_dict["zip_s3_uri"] = params["output_zip_s3_uri"]
 
     return results_dict

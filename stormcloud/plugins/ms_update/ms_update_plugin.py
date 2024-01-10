214c62072a714da8b5075a268d3b9c3bd13f6e0e
Nick Roberts <njroberts@dewberry.com>
2024-01-10 15:18:29 -0500

    Add upload of ranked documents and storm JSON s3 check

--- stormcloud/plugins/ms_update/ms_update_plugin.py
+++ stormcloud/plugins/ms_update/ms_update_plugin.py
@@ -1,9 +1,10 @@
 import json
 import logging
 import os
-from typing import Any, List
+from typing import Any, List, Tuple, Union
 
 import boto3
+from botocore.exceptions import HTTPClientError
 from create_ms_docs import SSTGeom, SSTMeta, SSTS3Document, SSTStart, SSTStats, create_ms_documents, sanitize_for_s3
 
 PLUGIN_PARAMS = {
@@ -13,6 +14,7 @@ PLUGIN_PARAMS = {
         "duration",
         "s3_bucket",
         "tropical_storm_json_s3_uri",
+        "ranked_events_json_s3_uri",
     ],
     "optional": [],
 }
@@ -22,20 +24,29 @@ def main(params: dict) -> str:
     logging.info(f"creating s3 client")
     session = boto3.session.Session(os.environ["AWS_ACCESS_KEY_ID"], os.environ["AWS_SECRET_ACCESS_KEY"])
     s3_client = session.client("s3")
+    tropical_bucket, tropical_key = split_uri(params["tropical_storm_json_s3_uri"])
+    # check that JSON for tropical storms exists before beginning processing
+    s3_client.head_object(Bucket=tropical_bucket, Key=tropical_key)
     logging.info(
         f"retrieving s3 documents associated with SST run for watershed {params['watershed_name']} and domain {params['transposition_domain']}"
     )
     s3_docs = get_s3_docs(
         s3_client, params["s3_bucket"], params["watershed_name"], params["transposition_domain"], params["duration"]
     )
-    bucket, *parts = params["tropical_storm_json_s3_uri"].replace("s3://", "").split("/")
-    key = "/".join(parts)
     logging.info(f"loading tropical storm data from s3 uri {params['tropical_storm_json_s3_uri']}")
-    tropical_storms_json = load_json(s3_client, bucket, key)
+    tropical_storms_json = load_json(s3_client, tropical_bucket, tropical_key)
     ms_docs = create_ms_documents(s3_docs, params["s3_bucket"], tropical_storms_json)
     ms_dict_list = [dict(m) for m in ms_docs]
-    ms_json_str = json.dumps(ms_dict_list)
-    return ms_json_str
+    ranked_bucket, ranked_key = split_uri(params["ranked_events_json_s3_uri"])
+    logging.info(f"Uploading ranked documents to s3 at uri {params['ranked_events_json_s3_uri']}")
+    json_str = upload_json(s3_client, ranked_bucket, ranked_key, ms_dict_list)
+    return json_str
+
+
+def split_uri(uri: str) -> Tuple[str, str]:
+    bucket, *parts = uri.replace("s3://", "").split("/")
+    key = "/".join(parts)
+    return bucket, key
 
 
 def load_json(client: Any, bucket: str, key: str) -> dict:
@@ -43,6 +54,12 @@ def load_json(client: Any, bucket: str, key: str) -> dict:
     text = res.get("Body").read().decode()
     data = json.loads(text)
     return data
+
+
+def upload_json(client: Any, bucket: str, key: str, data: Union[dict, list]) -> str:
+    json_str = json.dumps(data)
+    client.put_object(Body=json_str, Bucket=bucket, Key=key)
+    return json_str
 
 
 def get_s3_docs(

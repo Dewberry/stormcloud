57de8ed7d2333c16be0cd6196d3838b40531581b
Nick Roberts <njroberts@dewberry.com>
2024-01-12 11:25:33 -0500

    Give different message for entrypoint

--- stormcloud/plugins/doc_rank/entrypoint.sh
+++ stormcloud/plugins/doc_rank/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-echo 'StormViewer meilisearch database update plugin'
+echo 'StormViewer ranked doc creator'
 python -m main "{\"watershed_name\": \"trinity\", \"transposition_domain\": \"v01\", \"duration\": 72, \"s3_bucket\": \"tempest\", \"tropical_storm_json_s3_uri\": \"s3://tempest/watersheds/trinity/trinity_ibtracs.json\"}"

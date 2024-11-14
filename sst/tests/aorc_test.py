a6894a56ee232a25865ce11ae3ffe2aa52a7646a
Nick Roberts <njroberts@dewberry.com>
2024-11-13 21:00:38 -0500

    change to date with rain

--- sst/tests/aorc_test.py
+++ sst/tests/aorc_test.py
@@ -38,7 +38,7 @@ class AORCTestCase(unittest.TestCase):
             "Trinity",
             cls.subset_transposition_domain_geojson_fn,
             "v01",
-            datetime.datetime(2020, 1, 1),
+            datetime.datetime(2018, 9, 21),
             datetime.timedelta(hours=8),
             cls.test_directory.name,
         )
@@ -48,18 +48,18 @@ class AORCTestCase(unittest.TestCase):
         cls.test_directory.cleanup()
 
     def test_aorc_paths(self):
-        expected_s3_path_list = ["s3://noaa-nws-aorc-v1-1-1km/2020.zarr"]
+        expected_s3_path_list = ["s3://noaa-nws-aorc-v1-1-1km/2018.zarr"]
         self.assertListEqual(expected_s3_path_list, self.test_item.aorc_paths)
 
     def test_aorc_source_data(self):
-        expected_hash = "a268dd46a36def08828f476c3033233f33d11c26"
+        expected_hash = "04975bb58fd8690cab2a7bc8fd9ea043f9a56652"
         data_dict_hash = sha1(str(self.test_item.aorc_source_data.to_dict()).encode()).hexdigest()
         self.assertEqual(expected_hash, data_dict_hash)
 
     def test_aorc_sum_data(self):
-        expected_hash = "0376b50a72cd06166b70e1abdb75e2a3a294bc6d"
+        expected_hash = "4913ce18f44dd16773e33e73f6b10b4b1d86bc23"
         sum_dict_hash = sha1(str(self.test_item.sum_aorc.to_dict()).encode()).hexdigest()
-        self.assertEqual(sum_dict_hash, expected_hash)
+        self.assertEqual(expected_hash, sum_dict_hash)
 
     def test_valid_polygon(self):
         expected_valid_spaces_polygon = Polygon(
@@ -127,22 +127,21 @@ class AORCTestCase(unittest.TestCase):
     def test_max_transpose(self):
         expected_transpose_polygon = Polygon(
             [
-                [-97.26380950002647, 32.62866149999999],
-                [-97.26380950002647, 32.653660499999994],
-                [-97.2304775000265, 32.653660499999994],
-                [-97.2304775000265, 32.63699449999999],
-                [-97.23881050002649, 32.63699449999999],
-                [-97.23881050002649, 32.62866149999999],
-                [-97.26380950002647, 32.62866149999999],
+                [-97.18881250002653, 32.64532749999999],
+                [-97.18881250002653, 32.670326499999994],
+                [-97.15548050002656, 32.670326499999994],
+                [-97.15548050002656, 32.653660499999994],
+                [-97.16381350002655, 32.653660499999994],
+                [-97.16381350002655, 32.64532749999999],
+                [-97.18881250002653, 32.64532749999999],
             ]
         )
-        expected_affine_gdal_list = [1.0, 0.0, 0.0, 1.0, -0.05833099999995284, -0.016666000000000736]
+        expected_affine_gdal_list = [1.0, 0.0, 0.0, 1.0, 0.016665999999986525, 0.0]
         expected_stats_dict = {
-            "min": {"value": 0.0, "unit": "in"},
-            "mean": {"value": 0.0, "unit": "in"},
-            "max": {"value": 0.0, "unit": "in"},
-            "count": 12,
-            "normalized_mean": None,
+            "min": {"value": 0.04724409519218084, "unit": "in"},
+            "mean": {"value": 0.0508231933128006, "unit": "in"},
+            "max": {"value": 0.05118110312486258, "unit": "in"},
+            "count": 11,
         }
         poly, aff, stats = self.test_item.max_transpose(False)
         self.assertEqual(expected_transpose_polygon, poly)
@@ -150,15 +149,15 @@ class AORCTestCase(unittest.TestCase):
         self.assertDictEqual(expected_stats_dict, stats.to_dict())
 
     def test_max_transpose_properties(self):
+
         expected_properties_subset = {
             "sst:statistics": {
-                "min": {"value": 0.0, "unit": "in"},
-                "mean": {"value": 0.0, "unit": "in"},
-                "max": {"value": 0.0, "unit": "in"},
-                "count": 12,
-                "normalized_mean": None,
+                "min": {"value": 0.04724409519218084, "unit": "in"},
+                "mean": {"value": 0.0508231933128006, "unit": "in"},
+                "max": {"value": 0.05118110312486258, "unit": "in"},
+                "count": 11,
             },
-            "sst:transform": [1.0, 0.0, -0.05833099999995284, 0.0, 1.0, -0.016666000000000736],
+            "sst:transform": [1.0, 0.0, 0.016665999999986525, 0.0, 1.0, 0.0],
         }
         self.test_item.max_transpose(True)
         self.assertTrue(expected_properties_subset["sst:statistics"] == self.test_item.properties.get("sst:statistics"))
@@ -166,7 +165,7 @@ class AORCTestCase(unittest.TestCase):
         self.assertNotEqual(NULL_POLYGON, self.test_item.geometry)
 
     def test_thumbnail_asset(self):
-        self.test_item.aorc_thumbnail(1, True, True)
+        self.test_item.aorc_thumbnail(0.1, True, True)
         expected_asset_path = os.path.join(self.test_directory.name, "thumbnail.png")
         self.assertTrue(os.path.exists(expected_asset_path))
         self.assertTrue(self.test_item.assets.get("thumbnail") != None)

aa6abe061c77a56cb13d936588e208df8f3ba8a8
Stevenray Janke <sjanke@dewberry.com>
2024-11-20 16:59:39 -0500

    Update for new vars and storm types. Add multiday functionality

--- stormcloud/stormtyper/notebook_utils.py
+++ stormcloud/stormtyper/notebook_utils.py
@@ -6,7 +6,7 @@ import scrapbook as sb
 import pandas as pd
 
 # List of storm types
-storm_types = ["mid_latitude_cyclone", "mesoscale_storm", "tropical", "local_storm"]
+storm_types = ["Synoptic_Storm", "Mesoscale_Storm", "Atmospheric_River", "Local_Storm"]
 
 
 def init_stats_dict():
@@ -16,30 +16,43 @@ def init_stats_dict():
     # Initialize summary_stats dictionary
     summary_stats = {
         "PREC_ACC_NC": {},
-        "SBCAPE": {},
-        "SLP": {},
+        "IVT": {},
         "PWAT": {},
+        "SLP": {},
         "SRH": {},
+        "SBCAPE": {},
         "Z50000": {},
     }
 
-    # Common structure for each parameter
-    default_structure = {
-        "mid_latitude_cyclone": 0,
-        "mesoscale_storm": 0,
-        "tropical": 0,
-        "local_storm": 0,
-        "notes": None,
-    }
+    # Common storm type structure for each meteorological variable, initialized to 0
+    default_structure = {types: 0 for types in storm_types}
 
-    # Apply the common structure to each parameter in summary_stats
+    # Apply the common structure to each variable in summary_stats
     for param in summary_stats:
         summary_stats[param] = default_structure.copy()
 
     return summary_stats
 
 
-def create_select_widget(parameter, summary_stats):
+def init_notes_dict():
+    """
+    Build the summary stats dictionary used to track scoring for each storm
+    """
+    # Initialize summary_stats dictionary
+    summary_stats = {
+        "PREC_ACC_NC": {"notes": None},
+        "IVT": {"notes": None},
+        "PWAT": {"notes": None},
+        "SLP": {"notes": None},
+        "SRH": {"notes": None},
+        "SBCAPE": {"notes": None},
+        "Z50000": {"notes": None},
+    }
+
+    return summary_stats
+
+
+def create_select_widget(parameter, summary_stats, day):
     """
     Function to create and display a SelectMultiple widget for a given parameter
     """
@@ -50,7 +63,7 @@ def create_select_widget(parameter, summary_stats):
             for storm_type in storm_types
             if summary_stats[parameter][storm_type] == 1
         ],
-        description=f"{parameter} Storm Type:",
+        description=day,
         disabled=False,
     )
 
@@ -62,7 +75,7 @@ def create_select_widget(parameter, summary_stats):
             )
 
     select_widget.observe(update_dict, names="value")
-    display(select_widget)
+    return select_widget
 
 
 def create_notes_widget(parameter, summary_stats):
@@ -84,15 +97,13 @@ def create_notes_widget(parameter, summary_stats):
         summary_stats[parameter]["notes"] = change["new"]
 
     notes_widget.observe(update_notes, names="value")
-    display(notes_widget)
+    return notes_widget
 
 
-def storm_type_analysis(df: pd.DataFrame):
+def storm_type_analysis(df: pd.DataFrame, day: str):
     """
     Function to analyze storm type scores.
     """
-    # Removing the notes row
-    df = df.iloc[:-1]
     # Check if each parameter has values entered
     var_sums = df.sum(axis=0)
     for var, sum in var_sums.items():
@@ -101,7 +112,7 @@ def storm_type_analysis(df: pd.DataFrame):
 
     # Print total scores for each storm type
     storm_type_sums = df.sum(axis=1)
-    print("Total scores by category:\n")
+    print(f"{day} Total scores by category:\n")
     for storm_type, score in storm_type_sums.items():
         if storm_type != "notes":
             print(f"{storm_type}: {score}")
@@ -176,3 +187,73 @@ def convert_paths_to_html_links(data_frame, notebook_paths):
         lambda x: f"<a href='{x}'>{x}</a>"
     )
     return data_frame
+
+
+def create_multiday_dataframe(data):
+    results = {}
+    notes_combined = []
+
+    # Process each day
+    for day, attributes in data.items():
+        if day == "notes":  # Skip notes, handle separately
+            continue
+
+        storm_counts = {}
+        for attribute, storms in attributes.items():
+            for storm_type, count in storms.items():
+                if storm_type in storm_counts:
+                    storm_counts[storm_type] += count
+                else:
+                    storm_counts[storm_type] = count
+
+        # Determine the storm type with the maximum count
+        max_storm_type = max(storm_counts, key=storm_counts.get)
+        results[day] = max_storm_type
+
+    # Extract and combine notes
+    notes_dict = data.get("notes", {})
+    for attribute, info in notes_dict.items():
+        note = info.get("notes")
+        if note:
+            notes_combined.append(note)
+
+    # Create DataFrame
+    df = pd.DataFrame([results])
+    df["notes"] = ", ".join(filter(None, notes_combined))  # Join only non-empty notes
+
+    return df
+
+
+def process_multiday_notebooks(notebook_paths):
+    all_data_df = pd.DataFrame()
+    for path in notebook_paths:
+        storm_date = path.split("/")[-1].split(".")[0].split("-")[-1]
+        nb = sb.read_notebook(path)
+        scraps = nb.scraps
+        data = scraps["summary_stats"].data
+
+        # Use create_dataframe to process each notebooks data
+        df = create_multiday_dataframe(data)
+        df.insert(0, "storm_start_date", storm_date)
+
+        # Concat the new DataFrame to the collective DataFrame
+        all_data_df = pd.concat([all_data_df, df], ignore_index=True)
+
+    # Determine likely storm type for each row
+    def determine_storm_type(row):
+        if row.nunique() == 1:  # Check if all values in the row are the same
+            return row.iloc[0]  # Return the common value
+        else:
+            return "hybrid"
+
+    # Apply the function to each row considering only day columns
+    day_columns = [col for col in all_data_df.columns if "day_" in col]
+    all_data_df["likely_storm_type"] = all_data_df[day_columns].apply(
+        determine_storm_type, axis=1
+    )
+
+    # Move the notes column to the end
+    notes = all_data_df.pop("notes")
+    all_data_df["notes"] = notes
+
+    return all_data_df
